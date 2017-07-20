48d30889f4cdd1f627ac8ea6840059c79a643fb4
muyeby <bxf_hit@163.com>
2017-07-20 12:16:05 +0800

    fix bugs

--- wordembedding/utils/Predeal.py
+++ wordembedding/utils/Predeal.py
@@ -19,7 +19,7 @@ wordCountDir = ""
 originWordVec = ""
 OutPutDir = ""
 
-def Predeal(originFile,wordCountFile,lang,filters,newFile,cntFile):
+def Predeal(originFile,wordCountFile,lang,filters,newFile,cntFile,threshold):
     wordDict = {}
     wordDict2 = {}
     for line in open(wordCountFile):
@@ -30,21 +30,23 @@ def Predeal(originFile,wordCountFile,lang,filters,newFile,cntFile):
     file2 = open(newFile, 'wb')
     cntFile= open(cntFile,'wb')
     stopword=[]
-    if(filters==True):
+    if(filters==1):
         if(lang=="zh"):
-            with open('zh_stopwords.json', 'r') as f:
-                stopword = json.loads(f.read())
+            for line in open("vocab.zh",'r'):
+                word = line.strip()
+                stopword.append(word)
         if(lang=="en"):
             stopword = set(stopwords.words('english'))
     i=0
     if (file1):
 	print "start generate new vector file...\n"
+        #print filters
         for line in file1:
             if i!=0:
                 tmp = line.strip().split(' ')
 	        word = tmp[0]
-                if (wordDict.has_key(tmp[0]) and wordDict[tmp[0]]>1000):
-                    if(filters==True and word.decode('utf-8') in stopword):
+                if (wordDict.has_key(tmp[0]) and wordDict[tmp[0]]>threshold):
+                    if(lang== "zh" and filters==1 and word not in stopword):
                         continue
                     ttmp = line.strip()+"\n"
                     ttmp2 = word+" "+str(wordDict[tmp[0]])+"\n"
@@ -66,7 +68,8 @@ if __name__ == '__main__':
     parser.add_argument("-o", "--outputfile", type=str, help="Output file for predealed vectors")
     parser.add_argument("-o2", "--countfile", type=str, help="word count file for predealed vectors")
     parser.add_argument("-lang","--language", type=str, help="language")
-    parser.add_argument("-ft", "--filters", type=bool, help="whether filter stopwords")
+    parser.add_argument("-ft", "--filters", type=int, help="whether filter stopwords")
+    parser.add_argument("-th", "--threshold", type=int, help="threshold wordcounts")
     args = parser.parse_args()
     print "Do some predeal works\n"
-    Predeal(args.originFile,args.wordCountFile,args.language,args.filters,args.outputfile,args.countfile)
+    Predeal(args.originFile,args.wordCountFile,args.language,args.filters,args.outputfile,args.countfile,args.threshold)
